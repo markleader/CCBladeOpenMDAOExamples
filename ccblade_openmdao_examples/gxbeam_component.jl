1ccd8a3e13961ebdd2b5dccc492aaeff96a168f4
Mark Leader <markleader@gatech.edu>
2021-09-03 11:26:24 -0400

    AD working based on finite-difference check

--- ccblade_openmdao_examples/gxbeam_component.jl
+++ ccblade_openmdao_examples/gxbeam_component.jl
@@ -1,6 +1,7 @@
 using ConcreteStructs
 using ComponentArrays
 using ForwardDiff
+using StaticArrays
 using GXBeam, LinearAlgebra
 using OpenMDAO: AbstractExplicitComp
 
@@ -8,7 +9,8 @@ using OpenMDAO: AbstractExplicitComp
     rho
     E
     nu
-    points
+    span
+    nnodes
     apply_nonlinear_forwarddiffable!
     x
     y
@@ -25,7 +27,7 @@ function create_assembly(; points, Tp, Np, omega, chord, twist, A, Iyy, Izz, Iyz
 
     G = E/(2*(1 + nu))
 
-    x3c = 0.0 *cos.(twist).*chord  # 0.17*cos.(twist).*chord
+    x3c = 0.0 # 0.17*cos.(twist).*chord
     ky = 10*(1 + nu)/(12 + 11*nu)
     kz = 10*(1 + nu)/(12 + 11*nu)
 
@@ -36,8 +38,9 @@ function create_assembly(; points, Tp, Np, omega, chord, twist, A, Iyy, Izz, Iyz
     Ay = A./ky
     Az = A./kz
 
-    stiffness = fill(zeros(6,6), nelems)
-    mass = fill(zeros(6, 6), nelems)
+    T = eltype(A)
+    stiffness = fill(zeros(T, (6,6)), nelems)
+    mass = fill(zeros(T, (6, 6)), nelems)
 
     for i in 1:nelems
         elem_stiff = [[E*A[i], 0,       0,       0,  0,       0     ]#=
@@ -46,22 +49,22 @@ function create_assembly(; points, Tp, Np, omega, chord, twist, A, Iyy, Izz, Iyz
                     =#[0,      0,       0,       0,  0,       0     ]#=
                     =#[0,      0,       0,       0,  H22[i], -H23[i]]#=
                     =#[0,      0,       0,       0, -H23[i],  H33[i]]]
-        elem_mass = [[rho*A[i],        0,               0,        0,               rho*A[i]*x3c[i], 0     ]#=
-                   =#[0,               rho*A[i],        0,       -rho*A[i]*x3c[i], 0,               0     ]#=
+        elem_mass = [[rho*A[i],        0,               0,        0,               0,               0     ]#=
+                   =#[0,               rho*A[i],        0,        0,               0,               0     ]#=
                    =#[0,               0,               rho*A[i], 0,               0,               0     ]#=
-                   =#[0,              -rho*A[i]*x3c[i], 0,        Iyy[i]+Izz[i],   0,               0     ]#=
-                   =#[rho*A[i]*x3c[i], 0,               0,        0,               Iyy[i],         -Iyz[i]]#=
+                   =#[0,               0,               0,        Iyy[i]+Izz[i],   0,               0     ]#=
+                   =#[0,               0,               0,        0,               Iyy[i],         -Iyz[i]]#=
                    =#[0,               0,               0,        0,              -Iyz[i],          Izz[i]]]
         stiffness[i] = elem_stiff
         mass[i] = elem_mass
     end
 
-    # create assembly
+    # Convert the stiffness matrices to compliance matrices of the same type
     assembly = Assembly(points, start, stop, stiffness=stiffness, mass=mass)
 
     # set prescribed conditions (fixed left endpoint)
     bcs = Dict(
-        1 => PrescribedConditions(ux=0, uy=0, uz=0, theta_x=0, theta_y=0, theta_z=0)
+        1 => PrescribedConditions(ux=zero(T), uy=zero(T), uz=zero(T), theta_x=zero(T), theta_y=zero(T), theta_z=zero(T))
     )
 
     function interp_aero_force(x, radii, f_aero)
@@ -93,19 +96,15 @@ function create_assembly(; points, Tp, Np, omega, chord, twist, A, Iyy, Izz, Iyz
     system, converged = steady_state_analysis(assembly;
         prescribed_conditions=bcs,
         distributed_loads=distributed_loads,
-        angular_velocity=[0.0, 0.0, omega], linear=true)
+        angular_velocity=[zero(T), zero(T), omega], linear=true)
 
     return assembly, system
 end
 
-function SolverComp(; rho, E, nu, x)
+function SolverComp(; rho, E, nu, span, nnodes)
 
     # create the points
-    nnodes = length(x)
     nelems = nnodes-1
-    y = zero(x)
-    z = zero(x)
-    points = [[x[i], y[i], z[i]] for i = 1:nnodes]
 
     function apply_nonlinear_forwarddiffable!(y, x)
         T = eltype(x)
@@ -121,7 +120,17 @@ function SolverComp(; rho, E, nu, x)
         Izz = x[:Izz]
         Iyz = x[:Iyz]
 
-        assembly, system = create_assembly(points=self.points, omega=omega,
+        xpts = zeros(T, nnodes)
+        xvals = collect(range(0.06, span, length=nnodes))
+        for i = 1:nnodes
+            xpts[i] = xvals[i]
+        end
+
+        ypts = zero(xpts)
+        zpts = zero(xpts)
+        points = [[xpts[i], ypts[i], zpts[i]] for i = 1:nnodes]
+
+        assembly, system = create_assembly(points=points, omega=omega,
                                            Tp=Tp, Np=Np, chord=chord, twist=twist,
                                            A=A, Iyy=Iyy, Izz=Izz, Iyz=Iyz,
                                            rho=rho, E=E, nu=nu)
@@ -139,8 +148,7 @@ function SolverComp(; rho, E, nu, x)
         return nothing
     end
 
-    # Initialize the input and output vectors needed by ForwardDiff.jl. (The
-    # ForwardDiff.jl inputs include phi, but that's an OpenMDAO output.)
+    # Initialize the input and output vectors needed by ForwardDiff.jl.
     X = ComponentArray(
         omega=0.0, Np=zeros(Float64, nnodes), Tp=zeros(Float64, nnodes),
         chord=zeros(Float64, nelems), twist=zeros(Float64, nelems),
@@ -154,18 +162,18 @@ function SolverComp(; rho, E, nu, x)
     # the ForwardDiff.jacobian! function (apparently good for efficiency).
     config = ForwardDiff.JacobianConfig(apply_nonlinear_forwarddiffable!, Y, X)
 
-    return SolverComp(rho, E, nu, points, apply_nonlinear_forwarddiffable!, X, Y, J, config)
+    return SolverComp(rho, E, nu, span, nnodes, apply_nonlinear_forwarddiffable!, X, Y, J, config)
 end
 
 function OpenMDAO.setup(self::SolverComp)
 
-    nelems = length(self.points)-1
+    nelems = self.nnodes-1
 
     # Declare the OpenMDAO inputs.
     input_data = Vector{VarData}()
     push!(input_data, VarData("omega", shape=1, units="rad/s"))
-    push!(input_data, VarData("Tp", shape=nelems+1, units="N/m"))
-    push!(input_data, VarData("Np", shape=nelems+1, units="N/m"))
+    push!(input_data, VarData("Tp", shape=self.nnodes, units="N/m"))
+    push!(input_data, VarData("Np", shape=self.nnodes, units="N/m"))
     push!(input_data, VarData("chord", shape=nelems, units="m"))
     push!(input_data, VarData("twist", shape=nelems, units="rad"))
     push!(input_data, VarData("A", shape=nelems, units="m**2"))
@@ -225,7 +233,12 @@ function OpenMDAO.compute!(self::SolverComp, inputs, outputs)
     rho = self.rho
     nu = self.nu
 
-    assembly, system = create_assembly(points=self.points, omega=omega,
+    xpts = collect(range(0.06, self.span, length=self.nnodes))
+    ypts = zero(xpts)
+    zpts = zero(xpts)
+    points = [[xpts[i], ypts[i], zpts[i]] for i = 1:self.nnodes]
+
+    assembly, system = create_assembly(points=points, omega=omega,
                                        Tp=Tp, Np=Np, chord=chord, twist=twist,
                                        A=A, Iyy=Iyy, Izz=Izz, Iyz=Iyz,
                                        rho=rho, E=E, nu=nu)
@@ -235,9 +248,9 @@ function OpenMDAO.compute!(self::SolverComp, inputs, outputs)
     )
     state = AssemblyState(system, assembly; prescribed_conditions=bcs)
 
-    Fx[1:end] = [state.elements[ielem].F[1] for ielem = 1:length(assembly.elements)]
-    My[1:end] = [state.elements[ielem].M[2] for ielem = 1:length(assembly.elements)]
-    Mz[1:end] = [state.elements[ielem].M[3] for ielem = 1:length(assembly.elements)]
+    Fx .= [state.elements[ielem].F[1] for ielem = 1:length(assembly.elements)]
+    My .= [state.elements[ielem].M[2] for ielem = 1:length(assembly.elements)]
+    Mz .= [state.elements[ielem].M[3] for ielem = 1:length(assembly.elements)]
 
 end
 
@@ -255,8 +268,6 @@ function OpenMDAO.compute_partials!(self::SolverComp, inputs, partials)
     Iyz = inputs["Iyz"]
     nelems = length(A)
     nnodes = nelems+1
-
-    x_ce = ComponentArray(omega=omega, Np=Np, Tp=Tp, chord=chord, twist=twist, A=A, Iyy=Iyy, Izz=Izz, Iyz=Iyz)
 
     # Working arrays and configuration for ForwardDiff's Jacobian routine.
     x = self.x
@@ -276,23 +287,23 @@ function OpenMDAO.compute_partials!(self::SolverComp, inputs, partials)
 
     # Reshape the partials
     dFx_domega = partials["Fx", "omega"]
-    dFx_dA = partials["Fx", "A"] #transpose(reshape(partials["Fx", "A"], nelems, nelems))
+    dFx_dA = partials["Fx", "A"]
 
     dMy_domega = partials["My", "omega"]
-    dMy_dTp = partials["My", "A"] #transpose(reshape(partials["My", "Tp"], nelems, nnodes))
-    dMy_dNp = partials["My", "Np"] #transpose(reshape(partials["My", "Np"], nelems, nnodes))
-    dMy_dA = partials["My", "A"] #transpose(reshape(partials["My", "A"], nelems, nelems))
-    dMy_dIyy = partials["My", "Iyy"] #transpose(reshape(partials["My", "Iyy"], nelems, nelems))
-    dMy_dIzz = partials["My", "Izz"] #transpose(reshape(partials["My", "Izz"], nelems, nelems))
-    dMy_dIyz = partials["My", "Iyz"] #transpose(reshape(partials["My", "Iyz"], nelems, nelems))
+    dMy_dTp = partials["My", "Tp"]
+    dMy_dNp = partials["My", "Np"]
+    dMy_dA = partials["My", "A"]
+    dMy_dIyy = partials["My", "Iyy"]
+    dMy_dIzz = partials["My", "Izz"]
+    dMy_dIyz = partials["My", "Iyz"]
 
     dMz_domega = partials["Mz", "omega"]
-    dMz_dTp = partials["My", "Tp"] #transpose(reshape(partials["Mz", "Tp"], nelems, nnodes))
-    dMz_dNp = partials["My", "Np"] #transpose(reshape(partials["Mz", "Np"], nelems, nnodes))
-    dMz_dA = partials["My", "A"] #transpose(reshape(partials["Mz", "A"], nelems, nelems))
-    dMz_dIyy = partials["My", "Iyy"] #transpose(reshape(partials["Mz", "Iyy"], nelems, nelems))
-    dMz_dIzz = partials["My", "Izz"] #transpose(reshape(partials["Mz", "Izz"], nelems, nelems))
-    dMz_dIyz = partials["My", "Iyz"] #transpose(reshape(partials["Mz", "Iyz"], nelems, nelems))
+    dMz_dTp = partials["Mz", "Tp"]
+    dMz_dNp = partials["Mz", "Np"]
+    dMz_dA = partials["Mz", "A"]
+    dMz_dIyy = partials["Mz", "Iyy"]
+    dMz_dIzz = partials["Mz", "Izz"]
+    dMz_dIyz = partials["Mz", "Iyz"]
 
     # Get the Jacobian.
     ForwardDiff.jacobian!(J, self.apply_nonlinear_forwarddiffable!, y, x, config)
